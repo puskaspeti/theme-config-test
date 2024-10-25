0c6f64b2db99976f2326ca57ecadc590c0900999
Puskás Péter <puskas.peter@nextval.hu>
2024-10-25 20:46:36 +0200

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, MatButton],
+})
+export class HomePage {
+  constructor() {}
+}
